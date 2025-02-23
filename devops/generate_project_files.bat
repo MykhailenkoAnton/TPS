c6d29bd62edbb7ecf0001c1bf6636177f1573d18
anton.mykhailenko <anton.mykhailenko@fbt.loc>
2025-02-23 21:30:38 +0200

    generate for unreal engine 5.5.3

--- devops/generate_project_files.bat
+++ devops/generate_project_files.bat
@@ -5,7 +5,7 @@ set UBTRelativePath=%~2
 set VersionSelector=%~3
 set ProjectPath=%CD%\%~4
 
-%VersionSelector% -switchversionsilent "%ProjectPath%" "%EnginePath%"
-"%EnginePath%\%UBTRelativePath%" -projectfiles -progress -project="%ProjectPath%" -game -engine
+"%VersionSelector%" -switchversionsilent  "%ProjectPath%"  "%EnginePath%"
+::"%EnginePath%\%UBTRelativePath%" -projectfiles -progress -project="%ProjectPath%"  -game -engine
 
-
+pause
